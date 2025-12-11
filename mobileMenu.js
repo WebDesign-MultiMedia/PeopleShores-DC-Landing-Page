f4203523a9b71b2ab374214d23f568948c9b3da1
WebDesign-MultiMedia <salasjulio386@gmail.com>
2025-12-10 21:08:10 -0500

    location options added

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,12 +1,10 @@
 const btn = document.getElementById('mobile-menu-button');
 const menu = document.getElementById('mobile-menu');
-const iconOpen = document.getElementById('icon-open');
-const iconClose = document.getElementById('icon-close');
+const openIcon = document.getElementById('icon-open');
+const closeIcon = document.getElementById('icon-close');
 
 btn.addEventListener('click', () => {
-  const isOpen = btn.getAttribute('aria-expanded') === 'true';
-  btn.setAttribute('aria-expanded', String(!isOpen));
   menu.classList.toggle('hidden');
-  iconOpen.classList.toggle('hidden');
-  iconClose.classList.toggle('hidden');
+  openIcon.classList.toggle('hidden');
+  closeIcon.classList.toggle('hidden');
 });
